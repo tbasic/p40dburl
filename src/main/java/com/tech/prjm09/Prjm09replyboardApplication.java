1e2b29a2c5108001fee5af39debba1cd59a38e12
tbasic <tbasicapp@gmail.com>
2025-07-24 12:05:51 +0900

    11

---
+++ src/main/java/com/tech/prjm09/Prjm09replyboardApplication.java
@@ -0,0 +1,47 @@
+package com.tech.prjm09;
+
+import java.security.CodeSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prjm09replyboardApplication {
+	
+	
+//	public static void main(String[] args) {
+//		SpringApplication.run(Prjm09replyboardApplication.class, args);
+//		
+//		
+//	}
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Prjm09replyboardApplication.class);
+
+        // WAR 파일 경로 기반 프로파일 설정
+        String path = getWarLocation();
+        System.out.println("path : "+path);
+        if (path != null && path.contains("/app/")) {
+        	System.out.println("server server~~~~~~~");
+            app.setAdditionalProfiles("server");
+        } else {
+        	System.out.println("local local~~~~~~~");
+            app.setAdditionalProfiles("local");
+        }
+
+        app.run(args);
+    }
+
+    private static String getWarLocation() {
+        try {
+            CodeSource codeSource = Prjm09replyboardApplication.class.getProtectionDomain().getCodeSource();
+            if (codeSource != null) {
+                return codeSource.getLocation().getPath();
+            }
+        } catch (Exception e) {
+            // 로그로 출력해도 좋습니다.
+        }
+        return null;
+    }
+
+}
